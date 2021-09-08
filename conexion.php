d1a8e65def87d0561e493e21ef9211326c822a5d
santiago ceballos <43916807+THEGLUM@users.noreply.github.com>
2021-09-07 22:30:52 -0500

    sentencia registro

--- conexion.php
+++ conexion.php
@@ -1,68 +1,24 @@
 <?php
 
-    Class Conexion {
-        private $host = 'localhost';
-        private $user = 'root';
-        private $password = '';
-        private $db = 'formulario';
-        private $conn;
-/*         private $host = '10.96.16.53';
-        private $user = 'personalma';
-        private $password = 'personalma';
-        private $db = 'Empleados_Almacontact';
-        private $conn; */
+    $host = 'localhost';
+    $user = 'root';
+    $password = '';
+    $db = 'formulario';
+    $conn;
+/*  private $host = '10.96.16.53';
+    private $user = 'personalma';
+    private $password = 'personalma';
+    private $db = 'Empleados_Almacontact';
+    private $conn; */
 
-        public function __construct()
-        {
-            $conexionString = "mysql:host=".$this->host.";dbname=".$this->db.";charset=utf8";
-            try {
-                $this->conn = new PDO($conexionString,$this->user,$this->password);
-                $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-                    echo "conexion con la base de datos🥰";
-            } catch (Exception $e) {
-                $this->conn = 'error en la conexion';
-                echo "error:".$e->getMessage();
-            }
-        }
+    try {
+        $conexionString = "mysql:host=".$host.";dbname=".$db.";charset=utf8";
+        $conn = new PDO($conexionString,$user,$password);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            echo "conexion con la base de datos";
+    } catch (Exception $e) {
+        $conn = 'error en la conexion';
+        echo "error:".$e->getMessage()."line".$e->getLine();
     }
-    $conn =  new Conexion();
 
-$tipoDedocumento = $_POST["tipoDedocumento"];
-$numeroDeDocumento = $_POST["numeroDeDocumento"];
-$primerApellido = $_POST["PrimerApellido"];
-$segundoApellido = $_POST["segundoApellido"];
-$primerNombre = $_POST["primeroNombre"];
-$segundoNombre = $_POST["segundoNombre"];
-$area = $_POST["area"];
-$direccion = $_POST["direccion"];
-$interior = $_POST["flexRadioDefault"];
-$municipioRecidencia = $_POST["municipioRecidencia"];
-$barrio = $_POST["barrio"];
-$sedeLaboral = $_POST["sedeLaboral"];
-$telefonoFijo = $_POST["telefono"];
-$movil = $_POST["movil"];
-$telefonoEmergencia = $_POST["telefonoEmergencia"];
-$correo = $_POST["correo"];
-$fechaNacimiento = $_POST["fechaNacimiento"];
-$genero = $_POST["genero"];
-$idiomas = $_POST["idiomas"];
-
-echo "<br>"."$tipoDedocumento"." <br> ";
-echo "$numeroDeDocumento"." <br> ";
-echo "$primerApellido"." <br> ";
-echo "$segundoApellido"." <br> ";
-echo "$primerNombre"." <br> ";
-echo "$segundoNombre"." <br> ";
-echo "$area"." <br> ";
-echo "$direccion"." <br> ";
-echo "$interior"." <br> ";
-echo "$municipioRecidencia"." <br> ";
-echo "$barrio"." <br> ";
-echo "$sedeLaboral"." <br> ";
-echo "$telefonoFijo"." <br> ";
-echo "$movil"." <br> ";
-echo "$telefonoEmergencia"." <br> ";
-echo "$correo"." <br> ";
-echo "$fechaNacimiento"." <br> ";
-echo "$genero"." <br> ";
-echo "$idiomas"." <br> ";
+    return $conn;
