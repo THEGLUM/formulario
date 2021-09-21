a1cd8a4ca07a338d17fac33a34616488f0ee9a26
santiago ceballos <43916807+THEGLUM@users.noreply.github.com>
2021-09-20 23:11:55 -0500

    finalizacion de la primera parte

---
+++ formulario/barrio.php
@@ -0,0 +1,17 @@
+<?php
+
+$id = $_POST['id_municipio'];
+$html='';
+if(isset($id)){
+    $conn = include_once("./conexion.php");
+    $query = "SELECT * FROM Barrio WHERE Fk_id_municipio= ".$id;
+    $res = $conn -> prepare($query);
+    $res->execute();
+    $datos = $res->fetchAll();
+    $data = '';
+    foreach ($datos as $value) {
+        $data = $data . '<option value='.$value['Id_barrio'].'>'.$value['Nombre_barrio'].'</option>';
+    }
+    echo $data;
+}
+
--- formulario/formulario3.0/html/formulario.php
+++ formulario/formulario3.0/html/formulario.php
@@ -231,32 +231,14 @@
                   <label for="municipioDeRecidencia">Municipio de Recidencia*</label>
                   <select class="form-select form-select-sm" name="municipioRecidencia" required="true" aria-label=".form-select-sm example" id="municipioDeRecidencia">
                     <option selected value="">selecciona </option>
-                        <?php
-                        $select = include_once("../../conexion.php");
-                        $select = $conn->prepare("SELECT * FROM Municipio");
-                        $select->execute();
-                        $data = $select->fetchAll();
-                        foreach ($data as $valores):
-                        echo '<option value="'.$valores["Nombre_municipio"].'">'.$valores["Nombre_municipio"].'</option>';
-                        endforeach;
-                        ?>
                   </select>
                 </div>
 
 
                 <div class="caja">
                   <label for="barrio">Barrio*</label>
-                  <select class="form-select form-select-sm" name="barrio" required="true" aria-label=".form-select-sm example" id="municipioDeRecidencia">
+                  <select class="form-select form-select-sm" name="barrio" required="true" aria-label=".form-select-sm example" id="Barrio">
                     <option selected value="">selecciona </option>
-                    <?php
-                        $select = include_once("../../conexion.php");
-                        $select = $conn->prepare("SELECT * FROM Barrio");
-                        $select->execute();
-                        $data = $select->fetchAll();
-                        foreach ($data as $valores):
-                        echo '<option value="'.$valores["id_barrio"].'">'.$valores["Nombre_barrios"].'</option>';
-                        endforeach;
-                        ?>
                   </select>
                 </div>
 
@@ -351,8 +333,35 @@
     <script>
       $(document).ready(function(e) {
         $("#sedeLaboral").change(function(){
-          let id = $("sede").val()
-          alert(id)
+          let parametros = "id= "+$("#sedeLaboral").val()
+          $.ajax({
+            data: parametros,
+            url:  '../../municipio.php',
+            type: 'post',
+            beforeSend: function(){},
+            success: function(response){
+              $("#municipioDeRecidencia").html(response)
+            },
+            error: function(){
+              alert("error")
+            }
+          })
+        })
+
+        $("#municipioDeRecidencia").change(function(){
+          let parametros = "id_municipio= "+$("#municipioDeRecidencia").val()
+          $.ajax({
+            data: parametros,
+            url:  '../../barrio.php',
+            type: 'post',
+            beforeSend: function(){},
+            success: function(response){
+              $("#Barrio").html(response)
+            },
+            error: function(){
+              alert("error")
+            }
+          })
         })
 
       })
