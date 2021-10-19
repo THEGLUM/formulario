426f2c9ff99c46b1648401b232979b8a339ad9df
Esoto2 <edwin.velez1615@gmail.com>
2021-10-19 15:49:19 -0500

    Update validarDatos.php

--- formulario/validarDatos.php
+++ formulario/validarDatos.php
@@ -6,21 +6,25 @@ $conn = require_once('conexion.php');
     $CorreoElectronico = trim($_POST["txtCorreo"]);
     
   
-    
-    $basesdeDatos = $conn->prepare("SELECT * FROM Personal_almacontact WHERE Pk_NumeroDocumento = ? AND CorreoElectronico = ?;");
-    
-    $basesdeDatos->bindParam(1, $Fk_NumeroDocumento, PDO::PARAM_INT);
-    $basesdeDatos->bindParam(2, $CorreoElectronico, PDO::PARAM_STR);
-    $basesdeDatos-> execute();
-    var_dump($basesdeDatos);
-    $hola = $basesdeDatos;
-        var_dump($hola);
+    try {
+        $basesdeDatos = $conn->prepare("SELECT * FROM Personal_almacontact WHERE Pk_NumeroDocumento = ? AND CorreoElectronico = ?;");
         
-if ($basesdeDatos == true)
-{
-    echo "Funciona";   # code...
-}
-else
-{
-    echo "No funciona";
-}
+        $basesdeDatos->bindParam(1, $Fk_NumeroDocumento, PDO::PARAM_INT);
+        $basesdeDatos->bindParam(2, $CorreoElectronico, PDO::PARAM_STR);
+        $basesdeDatos-> execute();
+        var_dump($basesdeDatos);
+        $hola = $basesdeDatos;
+            var_dump($hola);
+            
+    if ($basesdeDatos == true)
+    {
+        echo "Funciona";   # code...
+    }
+    else
+    {
+        echo "No funciona";
+    }
+        //code...
+    } catch (PDOExeption $e) {
+        echo $e;
+    }
